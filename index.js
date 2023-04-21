73f4deb5903306ce215b0ee94cfae1842c6b5ff0
jigardonda56 <80046052+jigardonda56@users.noreply.github.com>
2023-04-21 19:03:25 -0400

    cores in index.js

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const app = express()
 const port = 5000
 //order of app.use metters
 app.use(cors())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.use(bodyParser.json({limit: "50mb"}));
